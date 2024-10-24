8f423f88542da1e2824cc51a07d4c9828b4dc2ee
Nam <NamTran2010>
2024-10-24 10:38:20 +0700

    update task 2

--- repos/employee/src/main/java/com/leap/employee/EmployeeApp.java
+++ repos/employee/src/main/java/com/leap/employee/EmployeeApp.java
@@ -14,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -21,6 +22,7 @@ import tech.jhipster.config.DefaultProfileUtil;
 import tech.jhipster.config.JHipsterConstants;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableJpaRepositories(basePackages = "com.leap.employee.repository")
 @EnableConfigurationProperties({ LiquibaseProperties.class, ApplicationProperties.class })
 public class EmployeeApp {
